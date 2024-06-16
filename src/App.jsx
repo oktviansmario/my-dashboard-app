30b93b5ba695ac8a18d3fd564c69d3128a76fd7d
Rio <oktaviansmario@gmail.com>
2024-06-16 10:30:51 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './components/shared/Layout';
 import Dashboard from './components/Dashboard';
 import Products from './components/Nftmarketplace';
